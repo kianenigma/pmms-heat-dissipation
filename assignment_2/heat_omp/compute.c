cde5326391e0163b57daf23c3a1270a98160875f
kianenigma <Kian.peymani@gmail.com>
2018-02-23 15:44:51 +0100

    stable with separate iteration of maxdiff #8

--- assignment_2/heat_omp/compute.c
+++ assignment_2/heat_omp/compute.c
@@ -9,11 +9,11 @@
 
 /* Does the reduction step and return if the convergence has setteled */
 static inline int fill_report(const struct parameters *p, struct results *r,
-                       size_t h, size_t w,
-                       double (*restrict a)[h][w],
-                       double (*restrict b)[h][w],
-                       double iter,
-                       struct timeval *before)
+                              size_t h, size_t w,
+                              double (*restrict a)[h][w],
+                              double (*restrict b)[h][w],
+                              double iter,
+                              struct timeval *before)
 {
     /* compute min/max/avg */
     double tmin = INFINITY, tmax = -INFINITY;
@@ -116,7 +116,7 @@ void do_compute(const struct parameters* p, struct results *r)
         do_copy(h, w, src);
 
         /* compute */
-        #pragma omp parallel for \
+#pragma omp parallel for \
         private(i, j)\
         schedule(guided)\
         num_threads(p->nthreads)
@@ -127,65 +127,60 @@ void do_compute(const struct parameters* p, struct results *r)
                 double restw = 1.0 - w;
 
                 (*dst)[i][j] = w * (*src)[i][j] +
-
                                ((*src)[i+1][j  ] + (*src)[i-1][j  ] +
                                 (*src)[i  ][j+1] + (*src)[i  ][j-1]) * (restw * c_cdir) +
-
                                ((*src)[i-1][j-1] + (*src)[i-1][j+1] +
                                 (*src)[i+1][j-1] + (*src)[i+1][j+1]) * (restw * c_cdiag);
 
             }
         }
 
-        if (iter % p->period == 0 ) {
-            #pragma omp parallel for private(i, j ) reduction(max: maxdiff) num_threads(p->nthreads)
+#pragma omp parallel for private(i, j ) reduction(max: maxdiff) num_threads(p->nthreads)
+        for (i = 1; i < h - 1; ++i) {
+            for (j = 1; j < w - 1; ++j) {
+                double v = (*dst)[i][j];
+                double v_old = (*src)[i][j];
+                double diff = fabs(v - v_old);
+                if (diff > maxdiff) maxdiff = diff;
+            }
+        }
+
+        if ( maxdiff < p->threshold ) { break; }
+
+        if ( p->printreports ) {
+            double tmin = INFINITY, tmax = -INFINITY;
+            double sum = 0.0;
+            struct timeval after;
+
+            /* We have said that the final reduction does not need to be included. */
+            gettimeofday(&after, NULL);
+
             for (i = 1; i < h - 1; ++i) {
                 for (j = 1; j < w - 1; ++j) {
                     double v = (*dst)[i][j];
                     double v_old = (*src)[i][j];
-                    double diff = fabs(v - v_old);
-                    if (diff > maxdiff) maxdiff = diff;
+
+                    sum += v;
+                    if (tmin > v) tmin = v;
+                    if (tmax < v) tmax = v;
                 }
             }
 
-            if ( maxdiff < p->threshold ) { break; }
+            r->niter = iter;
+            r->maxdiff = maxdiff;
+            r->tmin = tmin;
+            r->tmax = tmax;
+            r->tavg = sum / (p->N * p->M);
 
-            if ( p->printreports ) {
-                double tmin = INFINITY, tmax = -INFINITY;
-                double sum = 0.0;
-                struct timeval after;
+            r->time = (double)(after.tv_sec - before.tv_sec) +
+                      (double)(after.tv_usec - before.tv_usec) / 1e6;
 
-                /* We have said that the final reduction does not need to be included. */
-                gettimeofday(&after, NULL);
+            report_results(p, r);
 
-                for (i = 1; i < h - 1; ++i) {
-                    for (j = 1; j < w - 1; ++j) {
-                        double v = (*dst)[i][j];
-                        double v_old = (*src)[i][j];
-
-                        sum += v;
-                        if (tmin > v) tmin = v;
-                        if (tmax < v) tmax = v;
-                    }
-                }
-
-                r->niter = iter;
-                r->maxdiff = maxdiff;
-                r->tmin = tmin;
-                r->tmax = tmax;
-                r->tavg = sum / (p->N * p->M);
-
-                r->time = (double)(after.tv_sec - before.tv_sec) +
-                          (double)(after.tv_usec - before.tv_usec) / 1e6;
-
-                report_results(p, r);
-
-            }
         }
     }
 
     /* report at end in all cases */
-    iter--;
     fill_report(p, r, h, w, dst, src, iter, &before);
 
     free(c);
